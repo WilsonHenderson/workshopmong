5a89fc611f4c862298c762bfb5b7ed4db75e4ff9
Wilson Henderson <wilsonenderson01@gmail.com>
2026-05-06 10:25:06 -0300

    Conectando ao MongoDB com repository e Service

---
+++ src/main/java/com/wilsonfuro/workshopmongo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.wilsonfuro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wilsonfuro.workshopmongo.domain.User;
+import com.wilsonfuro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+	}
+
+}
