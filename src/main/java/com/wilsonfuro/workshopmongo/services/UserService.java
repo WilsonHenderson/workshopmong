953459a657841b1ab4a6776f19323ad934f2ba26
Wilson Henderson <wilsonenderson01@gmail.com>
2026-05-16 23:35:25 -0300

    obtendo um usuario por id

--- src/main/java/com/wilsonfuro/workshopmongo/services/UserService.java
+++ src/main/java/com/wilsonfuro/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wilsonfuro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wilsonfuro.workshopmongo.domain.User;
 import com.wilsonfuro.workshopmongo.repository.UserRepository;
+import com.wilsonfuro.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,17 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj  = repo.findById(id);
+		
+		User user = obj.orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 		
 	}
 
