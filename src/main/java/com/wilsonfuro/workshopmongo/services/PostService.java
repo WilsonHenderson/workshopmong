c87e535608eabee805e38aca30bf982e694018d8
Wilson Henderson <wilsonenderson01@gmail.com>
2026-07-26 19:58:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/wilsonfuro/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.wilsonfuro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wilsonfuro.workshopmongo.domain.Post;
+import com.wilsonfuro.workshopmongo.repository.PostRepository;
+import com.wilsonfuro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById  ( id);
+		Post post = obj.orElse(null);
+		if (post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return post;
+		
+	}
+
+}
