6f6b021ff24cb7edd63ec48afc016c9fac58efea
Wilson Henderson <wilsonenderson01@gmail.com>
2026-08-25 22:16:03 -0300

    Consulta simples com @Query

--- src/main/java/com/wilsonfuro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wilsonfuro/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.wilsonfuro.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wilsonfuro.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String> {
+	
+	  @Query("{ 'Title' : { $regex: ?0, $options: 'i' } } ")
+	 List<Post> serchTitle(String text);
 
 	
 	 List<Post> findByTitleContainingIgnoreCase(String text);
